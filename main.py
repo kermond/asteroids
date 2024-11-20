bcdafda062d21800a376adf87a89c21f76c8f952
kermond <kermond@gmail.com>
2024-11-19 23:51:07 -0500

    Add a player triagle and movement

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0, rect=None, special_flags=0)
+        screen.fill("black")
+        for thing_to_update in updateable:
+            thing_to_update.update(dt)
+        for thing_to_draw in drawable:
+            thing_to_draw.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
